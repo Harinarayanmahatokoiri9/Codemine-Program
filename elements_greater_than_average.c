2471c3c1e8ab179d83d71f655972382b4292ac11
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:24:50 +0530

    Create elements_greater_than_average.c

---
+++ elements_greater_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int s,i,arr[s],sum=0;
+    scanf("%d",&s);
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    int avg,count=0;
+    avg=sum/s;
+    for(i=0;i<s;i++)
+    {
+        if(arr[i]>=avg)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
