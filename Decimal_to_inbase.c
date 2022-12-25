fb9309eeba6d053ca9bb35dbb8c3ec1950a0647d
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:24:05 +0530

    Create Decimal_to_inbase.c

---
+++ Decimal_to_inbase.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,res=0,once=0,a,max=0,count=0;
+    scanf("%d%d",&n,&d);
+    while(n)
+    {
+        a=n%d;
+        n=n/d;
+        if(a==0)
+        {
+            once=1;
+            res++;
+            count=res;
+        }
+        else
+        {
+            if(count>max)
+            {
+                max=count;
+            }
+            res=0;
+        }
+    }
+    if(once==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",count);
+    }
+}
