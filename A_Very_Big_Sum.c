378974e1e48af03feca30b4c23c1d2a14ae18535
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:21:09 +0530

    Create A_Very_Big_Sum.c

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    long long int n,i,sum=0;
+    scanf("%lli",&n);
+    int arr[i];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("%lli",sum);
+}
