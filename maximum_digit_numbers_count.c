a4ecfe6444a8d173ab3e3181881e14ff887a189a
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:28:30 +0530

    Create maximum_digit_numbers_count.c

---
+++ maximum_digit_numbers_count.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,count,max=0;
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        a=arr[i];
+        if(a==0)
+        {
+            count++;
+        }
+        while(a)
+        {
+            count++;
+            a=a/10;
+        }
+        if(max<count)
+        {
+            max=count;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        a=arr[i];
+        if(a==0)
+        {
+            count++;
+        }
+        while(a)
+        {
+            count++;
+            a=a/10;
+        }
+        if(count==max)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+}
