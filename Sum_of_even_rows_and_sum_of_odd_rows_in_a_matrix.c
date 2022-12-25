82b4ac79281e662ce8b689e1c68095a22acfcd1c
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:33:13 +0530

    Create Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,sume=0,sumo=0;
+    scanf("%d%d",&a,&b);
+    int arr[a][b];
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            if(i%2==0)
+            {
+                sume+=arr[i][j];
+            }
+            else
+            {
+                sumo+=arr[i][j];
+            }
+        }
+    }
+    printf("%d %d",sume,sumo);
+}
