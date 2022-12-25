bc44eea2eb207a7603ab1089ecb2c4610cf49d8e
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:34:03 +0530

    Create without_spaces_number_of_characters.c

---
+++ without_spaces_number_of_characters.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[10000];
+    int i,c=0;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]!=' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
