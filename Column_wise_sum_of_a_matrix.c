c44ebab40222bad51f6f4fc8f854ca967a5fc821
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:23:06 +0530

    Create Column_wise_sum_of_a_matrix.c

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,sum,j,max=0;
+    scanf("%d%d",&a,&b);
+    int arr[a][b];
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(j=0;j<b;j++)
+    {
+        sum=0;
+        for(i=0;i<a;i++)
+        {
+            sum+=arr[i][j];
+        }
+        printf("%d ",sum);
+    }
+}
