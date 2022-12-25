c584e0a6de55c39d41e0c874c8f01988477fbdc1
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:29:58 +0530

    Create perfect_square_root_or_not.c

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    float f;
+    int a;
+    f=sqrt(n);
+    a=f;
+    if(a==f)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
