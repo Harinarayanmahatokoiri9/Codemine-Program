9f3f90598724f5b2b8c02fd0a41a801eff88323f
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:31:44 +0530

    Create reverse_of_a_numbers.c

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,r,sum=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum*10+r;
+        n/=10;
+    }
+    printf("%d",sum);
+}
