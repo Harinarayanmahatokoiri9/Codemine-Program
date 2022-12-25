23edc4118dd19afa1e2e64a6088eadfaaa4116f8
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:25:14 +0530

    Create even_odd_pairs.c

---
+++ even_odd_pairs.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,arr[n],j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(arr[j]%2==0 && arr[j]!=0)
+            {
+                printf("%d ",arr[j]);
+                arr[j]=0;
+                break;
+            }
+        }
+        for(j=0;j<n;j++)
+        {
+            if(arr[j]%2!=0 && arr[j]!=0)
+            {
+                printf("%d ",arr[j]);
+                arr[j]=0;
+                break;
+            }
+        }
+    }
+    if(n%2!=0)
+    {
+        printf("0");
+    }
+}
