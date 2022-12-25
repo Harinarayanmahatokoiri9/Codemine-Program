37859d835349b6a3350029b680e46b8a338dbde0
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:24:58 +0530

    Create Elements_same_as_count.c

---
+++ Elements_same_as_count.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c,count=0;
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(arr[i]==c)
+        {
+            count++;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i!=j)
+                {
+                    arr[j]=0;
+                }
+            }
+            printf("%d ",arr[i]);
+        }
+    }
+    if(count==0)
+    {
+        printf("-1");
+    }
+}
