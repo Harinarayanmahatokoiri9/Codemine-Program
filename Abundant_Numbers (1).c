43b272720c60ada1d9714f473c686f3ad0c198d5
Hari Narayan Mahato Koiri <22mh1a05h3@acoe.edu.in>
2022-12-25 18:21:14 +0530

    Create Abundant_Numbers (1).c

---
+++ Abundant_Numbers (1).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum>n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
